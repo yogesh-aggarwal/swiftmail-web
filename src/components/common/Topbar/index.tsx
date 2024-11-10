f3b1b8939a0dca4bed5b65e45d729b25ee342c32
Yogesh Aggarwal <yogeshdevaggarwal@gmail.com>
2024-11-10 22:21:21 +0530

    Refactor Topbar component to use 'actions' prop instead of 'routes' in multiple route files

--- src/components/common/Topbar/index.tsx
+++ src/components/common/Topbar/index.tsx
@@ -1,3 +1,12 @@
-export default function Topbar(props: { routes: { name: string; badge: string; onClick: () => any }[] }) {
-   return <header className="">Topbar component</header>
+export default function Topbar(props: { actions: { name: string; badge: string; onClick: () => any }[] }) {
+   return (
+      <header className="w-full">
+         {props.actions.map((action) => (
+            <div key={action.name} onClick={action.onClick}>
+               <span>{action.name}</span>
+               {action.badge && <span>{action.badge}</span>}
+            </div>
+         ))}
+      </header>
+   )
 }
--- src/components/routes/Attachments.tsx
+++ src/components/routes/Attachments.tsx
@@ -4,7 +4,7 @@ import Topbar from "@components/common/Topbar"
 import RouteLayout from "@layouts/RouteLayout"
 
 export default function Attachments() {
-   const topbar = useMemo(() => <Topbar routes={[]} />, [])
+   const topbar = useMemo(() => <Topbar actions={[]} />, [])
 
    return <RouteLayout topbar={topbar}></RouteLayout>
 }
