0a21f842200cfef369061e13c72bfc8ac621b803
Yogesh Aggarwal <yogeshdevaggarwal@gmail.com>
2024-11-10 22:49:47 +0530

    Enhance Topbar component with active state support and additional actions

--- src/components/common/Topbar/index.tsx
+++ src/components/common/Topbar/index.tsx
@@ -1,12 +1,43 @@
-export default function Topbar(props: { actions: { name: string; badge: string; onClick: () => any }[] }) {
+import { Search, Settings, Stars, VenetianMask } from "lucide-react"
+
+export default function Topbar(props: {
+   actions: { name: string; badge: string; active?: boolean; onClick: () => any }[]
+}) {
    return (
-      <header className="w-full">
-         {props.actions.map((action) => (
-            <div key={action.name} onClick={action.onClick}>
-               <span>{action.name}</span>
-               {action.badge && <span>{action.badge}</span>}
+      <header className="grid grid-cols-[min-content,1fr,2fr] gap-2 p-2 pr-0">
+         <div className="flex items-center gap-[.4rem]">
+            {props.actions.map((action) => (
+               <div
+                  key={action.name}
+                  onClick={action.onClick}
+                  className={
+                     "flex h-[48px] cursor-pointer items-center justify-center rounded-[1.3rem] px-5 " +
+                     (action.active ? "bg-primary" : "bg-white")
+                  }
+               >
+                  <div className="items-top flex gap-2">
+                     <span className="whitespace-nowrap text-[.95rem] font-medium">{action.name}</span>
+                     {action.badge && <span className="text-[12px]">{action.badge}</span>}
+                  </div>
+               </div>
+            ))}
+         </div>
+         <div></div>
+         <div className="flex items-center gap-[.4rem]">
+            <div className="flex h-[48px] w-full items-center gap-4 rounded-[1.3rem] bg-white px-5">
+               <input type="text" className="w-full outline-none" placeholder="Search" />
+               <Search size={18} />
             </div>
-         ))}
+            <div className="flex aspect-[1.1] h-[48px] cursor-pointer items-center justify-center gap-4 rounded-[1.3rem] bg-white">
+               <VenetianMask size={20} />
+            </div>
+            <div className="flex aspect-[1.1] h-[48px] cursor-pointer items-center justify-center gap-4 rounded-[1.3rem] bg-white">
+               <Stars size={18} />
+            </div>
+            <div className="flex aspect-[1.1] h-[48px] cursor-pointer items-center justify-center gap-4 rounded-[1.3rem] rounded-br-none rounded-tr-none bg-black text-white">
+               <Settings size={20} />
+            </div>
+         </div>
       </header>
    )
 }
--- src/components/routes/Inbox.tsx
+++ src/components/routes/Inbox.tsx
@@ -4,7 +4,19 @@ import Topbar from "@components/common/Topbar"
 import RouteLayout from "@layouts/RouteLayout"
 
 export default function Inbox() {
-   const topbar = useMemo(() => <Topbar actions={[]} />, [])
+   const topbar = useMemo(
+      () => (
+         <Topbar
+            actions={[
+               { name: "Primary", badge: "3", onClick: () => alert("Primary"), active: true },
+               { name: "Social", badge: "3", onClick: () => alert("Social") },
+               { name: "Promotional", badge: "3", onClick: () => alert("Promotional") },
+               { name: "Updates", badge: "3", onClick: () => alert("Updates") },
+            ]}
+         />
+      ),
+      []
+   )
 
    return <RouteLayout topbar={topbar}></RouteLayout>
 }
