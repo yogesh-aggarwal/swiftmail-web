e63086b4ae2ccc1a49b730840b5faac940bc9a26
Yogesh Aggarwal <yogeshdevaggarwal@gmail.com>
2024-11-10 23:03:20 +0530

    Enhance Inbox component by adding search functionality and secondary actions to Topbar

--- src/components/routes/Inbox.tsx
+++ src/components/routes/Inbox.tsx
@@ -2,16 +2,27 @@ import { useMemo } from "react"
 
 import Topbar from "@components/common/Topbar"
 import RouteLayout from "@layouts/RouteLayout"
+import { Stars, VenetianMask } from "lucide-react"
 
 export default function Inbox() {
    const topbar = useMemo(
       () => (
          <Topbar
+            settings
             actions={[
                { name: "Primary", badge: "3", onClick: () => alert("Primary"), active: true },
                { name: "Social", badge: "3", onClick: () => alert("Social") },
                { name: "Promotional", badge: "3", onClick: () => alert("Promotional") },
                { name: "Updates", badge: "3", onClick: () => alert("Updates") },
+            ]}
+            search={{
+               placeholder: "Search in emails",
+               onSearch: (query) => console.log(query),
+               onKeyUp: (e) => e.key === "Enter" && console.log("Search"),
+            }}
+            secondaryActions={[
+               { name: "Ask AI", icon: <Stars size={18} />, onClick: () => alert("Refresh") },
+               { name: "Anonymous mode", icon: <VenetianMask size={20} />, onClick: () => alert("More") },
             ]}
          />
       ),
