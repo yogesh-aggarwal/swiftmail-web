806fdd9ed13decd77d4fafe48fb3d4185c3909d9
Yogesh Aggarwal <yogeshdevaggarwal@gmail.com>
2024-11-10 23:21:38 +0530

    Add CSS variables for primary and background colors in main.css and update Tailwind config to use them

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-	theme: {
-		extend: {
-			colors: {
-				background: "hsl(300, 4.3%, 95.5%)",
-				primary: "hsl(69.7, 98.8%, 66.9%)",
-				"purple-bg": "hsl(240, 40.5%, 92.7%)",
-				"gray-bg": "hsl(240, 3%, 93.5%)",
-				"green-bg": "hsl(109.4, 27.9%, 88%)",
-			},
-		},
-	},
-	plugins: [],
+   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+   theme: {
+      extend: {
+         colors: {
+            background: "var(--background)",
+            primary: "var(--primary)",
+            "purple-bg": "hsl(240, 40.5%, 92.7%)",
+            "gray-bg": "hsl(240, 3%, 93.5%)",
+            "green-bg": "hsl(109.4, 27.9%, 88%)",
+         },
+      },
+   },
+   plugins: [],
 }
