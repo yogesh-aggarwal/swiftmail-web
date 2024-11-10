a622ac10f19f5e520e318ffd437e7b674d4482ea
Yogesh Aggarwal <yogeshdevaggarwal@gmail.com>
2024-11-10 22:17:45 +0530

    Add background color to BaseLayout for improved visual consistency

--- src/components/layouts/BaseLayout/index.tsx
+++ src/components/layouts/BaseLayout/index.tsx
@@ -2,7 +2,7 @@ import Leftbar from "@components/common/Leftbar"
 
 export default function BaseLayout(props: { children: React.ReactNode }) {
    return (
-      <div className="grid grid-cols-[min-content,1fr] h-screen overflow-auto">
+      <div className="grid grid-cols-[min-content,1fr] h-screen overflow-auto bg-background">
          <Leftbar />
          <div className="h-full overflow-auto">{props.children}</div>
       </div>
