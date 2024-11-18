81e3b3096d113c2b6edcf9e9be06c02dbd2ddd52
Yogesh Aggarwal <yogeshdevaggarwal@gmail.com>
2024-11-19 01:03:36 +0530

    Refactor Inbox component to improve state management, update message handling, and enhance type safety

--- src/components/routes/Inbox.tsx
+++ src/components/routes/Inbox.tsx
@@ -1,19 +1,18 @@
 import { CheckCheck, Filter, LucideStar, RefreshCcw, Stars, VenetianMask } from "lucide-react"
-import { Outlet } from "react-router-dom"
+import { Outlet, useParams } from "react-router-dom"
 
-import Topbar from "@components/common/Topbar"
+import Topbar, { TopbarPropsAction } from "@components/common/Topbar"
+import { useRouter } from "@hooks/useRouter"
 import RouteLayout from "@layouts/RouteLayout"
-import { Message } from "@models/message"
 import { Manip } from "@utils/manip"
 import { classNames } from "@utils/ui"
 import { makeStore, useBoundValue } from "common-react-toolkit"
-import { messagesStore, userStore } from "src/lib/state"
+import { useMemo } from "react"
+import { useInbox, userStore } from "src/lib/state"
+import { Thread } from "src/lib/types/thread"
 
 const [uiStore, useUI] = makeStore<{
    categoryID: string
-   unreadCount: number
-   categories: { id: string; name: string; unreadCount: number }[]
-   messages: Record<string, Message[]>
    filter: {
       date_start: number
       date_end: number
@@ -23,9 +22,6 @@ const [uiStore, useUI] = makeStore<{
 }>(
    {
       categoryID: "",
-      unreadCount: 0,
-      categories: [],
-      messages: {},
       filter: {
          date_start: 0,
          date_end: 0,
@@ -39,18 +35,29 @@ const [uiStore, useUI] = makeStore<{
 
 namespace Components {
    export function Header() {
+      const inbox = useInbox()
       const categoryID = useUI((x) => x.categoryID)
-      const categories = useUI((x) => x.categories)
+
+      const categoryActions = useMemo(() => {
+         if (!inbox) return []
+
+         const result: Record<string, TopbarPropsAction> = {}
+         for (const [name, category] of Object.entries(inbox?.categories ?? {})) {
+            result[name] = {
+               name: name,
+               badge: category.unreadCount ? category.unreadCount.toString() : undefined,
+               onClick: () => uiStore.merge({ categoryID: name }),
+               active: name === categoryID,
+            }
+         }
+
+         return inbox.categoryOrder.map((x) => result[x])
+      }, [inbox, categoryID])
 
       return (
          <Topbar
             settings
-            actions={categories.map((x) => ({
-               name: x.name,
-               badge: x.unreadCount ? x.unreadCount.toString() : undefined,
-               onClick: () => uiStore.merge({ categoryID: x.id }),
-               active: x.id === categoryID,
-            }))}
+            actions={categoryActions}
             search={{
                placeholder: "Search in emails",
                onSearch: (query) => console.log(query),
@@ -64,12 +71,16 @@ namespace Components {
       )
    }
 
-   function Card(props: { meta: Message }) {
+   function Card(props: { meta: Thread }) {
+      const id = (useParams().id as string) ?? ""
+      const router = useRouter()
+
       return (
          <div
+            onClick={() => router(`/inbox/${props.meta._id}`)}
             className={classNames(
                "cursor-pointer rounded-xl p-3",
-               false ? "bg-purple-bg hover:bg-purple-bg" : "hover:bg-gray-bg"
+               id == props.meta._id ? "bg-purple-bg hover:bg-purple-bg" : "hover:bg-gray-bg"
             )}
          >
             <div className="flex items-center justify-between">
@@ -86,13 +97,11 @@ namespace Components {
                   }).format(props.meta.date_updated)}
                </div>
             </div>
-            <div className="pt-[2px] text-[.85rem] opacity-75">{props.meta.email_data.subject}</div>
-            <div className="pt-[2px] text-[.85rem] opacity-55">{props.meta.email_data.html_content}</div>
+            <div className="pt-[2px] text-[.85rem] opacity-75">{props.meta.title}</div>
+            <div className="pt-[2px] text-[.85rem] opacity-55">{props.meta.title}</div>
             <div className="mt-2 flex items-center gap-2">
                <div className="rounded-lg bg-green-bg px-2 py-1 text-[.7rem]">
-                  <span className="opacity-70">
-                     {Manip.toTitleCase(props.meta.priorities.at(-1)!)} priority
-                  </span>
+                  <span className="opacity-70">{Manip.toTitleCase(props.meta.priority)} priority</span>
                </div>
             </div>
          </div>
@@ -100,33 +109,40 @@ namespace Components {
    }
 
    export function Messages() {
-      const sections = useUI((x) => x.messages)
+      const selectedCategory = useUI((x) => x.categoryID)
+      const sections = useInbox((x) => x?.categories?.[selectedCategory]?.subcategories, [selectedCategory])
+
+      if (!sections) return <div className="flex h-full items-center justify-center">No messages</div>
 
       return (
-         <>
-            {Object.entries(sections).map(([name, messages]) => (
-               <div key={name} className="mb-4">
-                  <div className="mb-2 flex h-[48px] items-center rounded-xl bg-gray-100 px-3 text-[1rem] font-medium opacity-80">
-                     {name}
-                  </div>
-                  {messages.map((x) => (
-                     <Card key={x.id} meta={x} />
-                  ))}
-               </div>
-            ))}
+         <div className="flex h-full flex-col gap-1 overflow-y-auto p-2 pt-0">
+            {Object.entries(sections).map(
+               ([name, threads]) =>
+                  threads.length > 0 && (
+                     <div key={name} className="mb-4">
+                        <div className="mb-2 flex h-[48px] items-center rounded-xl bg-gray-100 px-3 text-[1rem] font-medium opacity-80">
+                           {name}
+                        </div>
+                        {threads.map((x) => (
+                           <Card key={x._id} meta={x} />
+                        ))}
+                     </div>
+                  )
+            )}
             <div className="flex min-h-[52px] items-center justify-center text-sm opacity-30">
                You have reached the end
             </div>
-         </>
+         </div>
       )
    }
 }
 
 export default function Inbox() {
-   const userID = userStore.value()?.id
+   const userID = userStore.value()?._id
    if (!userID) return
 
-   const unreadCount = useUI((x) => x.unreadCount)
+   const isLoading = false
+   const unreadCount = useInbox((x) => x?.unreadCount ?? 0)
 
    /* If no category is selected, select the first one */
    useBoundValue(() => {
@@ -138,53 +154,6 @@ export default function Inbox() {
 
       uiStore.merge({ categoryID: categories[0] })
    }, [userStore, uiStore])
-
-   /* Calculate the unread count */
-   useBoundValue(() => {
-      const messages = messagesStore.value()
-
-      /* Count unreads */
-      const messageDistribution: Record<string, string[]> = {}
-      for (const message of Object.values(messages)) {
-         for (const category of message.categories) {
-            if (!messageDistribution[category]) messageDistribution[category] = []
-            messageDistribution[category].push(message.id)
-         }
-      }
-      const categories = (userStore.value()?.data?.preferences?.inbox?.categories ?? []).map((x) => ({
-         id: x,
-         name: x,
-         unreadCount: messageDistribution[x]?.filter((x) => !messages[x].flags.is_read).length || 0,
-      }))
-
-      /* Update the store */
-      uiStore.merge({ categories: categories })
-   }, [userStore, messagesStore])
-
-   /* Update the messages */
-   useBoundValue(() => {
-      /* Filter out messages */
-      const categoryID = uiStore.value().categoryID
-      const unreadCount = Object.values(messagesStore.value()).filter((x) => !x.flags.is_read).length
-      const messages = Object.values(messagesStore.value()).filter((x) => x.categories.includes(categoryID))
-
-      const sections: Record<string, Message[]> = {
-         Unread: [],
-         "Everything else": [],
-      }
-      for (const message of messages) {
-         if (message.flags.is_read) {
-            sections["Everything else"].push(message)
-         } else {
-            sections["Unread"].push(message)
-         }
-      }
-
-      /* Update the store */
-      uiStore.merge({ unreadCount: unreadCount, messages: sections })
-   }, [uiStore, messagesStore])
-
-   const isLoading = false
 
    return (
       <RouteLayout topbar={<Components.Header />} className="grid grid-cols-[min-content,1fr] gap-2">
@@ -217,9 +186,7 @@ export default function Inbox() {
                   </div>
                </div> */}
             </div>
-            <div className="flex h-full flex-col gap-1 overflow-y-auto p-2 pt-0">
-               <Components.Messages />
-            </div>
+            <Components.Messages />
          </div>
          <div className="h-full w-full overflow-y-auto rounded-[1.6rem] bg-white">
             <Outlet />
